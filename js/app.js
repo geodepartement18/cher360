1c91e223d61471f0428e806e1a69267e1b9b856a
geodepartement18 <114421205+geodepartement18@users.noreply.github.com>
2026-08-20 14:51:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -106,7 +106,7 @@ async function loadAllGroupItems() {
   const num = 10;
   const items = [];
 
-  while (start !== -1 && items.length < items.length) {
+  while (start !== -1 && items.length < 4) {
     const result = await state.portal.queryItems({
       query: `group:${CONFIG.groupId}`,
       start,
